5b3885744053413e7147966128fc91127fdcd58e
saeedsaeedi2898 <33893454+saeedsaeedi2898@users.noreply.github.com>
2018-02-07 13:37:57 +0330

    Update tarfand.lua

--- bot/tarfand.lua
+++ bot/tarfand.lua
@@ -246,7 +246,7 @@ function create_config( )
     "note"
     
     },
-    sudo_users = {491861044,532461732,tonumber(our_id)},--Sudo users
+    sudo_users = {455694756,455694756,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 ]],
